e3d484c3af7718d339b7031f092bed60a435c0a5
renjithpta <renjithkn@gmail.com>
2020-04-02 19:38:16 +0530

    first commit

---
+++ raft-fabric-sample/web-app/server/printKeyStore.sh
@@ -0,0 +1,8 @@
+echo 'printing keystore for Birtishairways'
+
+ls ../../first-network/crypto-config/peerOrganizations/birtishairways.ibs.aero/users/Admin\@birtishairways.ibs.aero/msp/keystore/
+
+echo 'printing keystore for Gatewick'
+
+ls ../../first-network/crypto-config/peerOrganizations/gatewick.ibs.aero/users/Admin\@gatewick.ibs.aero/msp/keystore/
+
